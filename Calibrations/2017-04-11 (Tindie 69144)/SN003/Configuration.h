d7dba4a296e45bf9d73214fa0f961ff034136412
Jakub Polonský <jpolonsky+github@gmail.com>
2017-04-11 22:43:37 +0200

    Initial calibration files for SN003 and SN004

---
+++ Calibrations/2017-04-11 (Tindie 69144)/SN003/Configuration.h
@@ -0,0 +1,78 @@
+/**
+ * Configuration.h - Arduino setting and calibration constants
+ *
+ * 2017-04-04
+ * kaktus circuits
+ * GNU GPL v.3
+ */
+ 
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+/** 
+ * UNCOMMENT THE LINE THAT BELONGS TO YOUR ARDUINO VERSION, COMMENT THE OTHER LINE
+ * If you are using Arduino Zero (M0/M0 Pro), the sketch was tested with Arduino.org board.
+ */
+ 
+//#define ZERO /* Arduino Zero, M0 and M0 Pro (Cortex M0+, 32-bit) via Native port (USB) */
+#define UNO /* Arduino Uno (UNO, 8-bit) */
+
+
+/* Serial port */
+
+#if (defined(ZERO) && defined(UNO))
+  #error Only one type of board can be defined
+#elif defined(ZERO)
+  #define SerialPort SerialUSB
+#elif defined(UNO)
+  #define SerialPort Serial
+#else
+  #error No board defined
+#endif
+
+
+/* Calibration */
+
+#define CALIBRATION_DATE 					"2017-04-11"
+
+#define CURRENTSETTER_SLOPE_HI 				11122606L
+#define CURRENTSETTER_OFFSET_HI 			331321L
+#define CURRENTSETTER_SLOPE_LO 				1390061L
+#define CURRENTSETTER_OFFSET_LO 			41020L
+
+#define AMMETER_SLOPE_HI 					11127288L
+#define AMMETER_OFFSET_HI 					-789L
+#define AMMETER_SLOPE_LO 					1390641L
+#define AMMETER_OFFSET_LO 					301L
+
+#define VOLTSETTER_SLOPE_HI 				31996201L
+#define VOLTSETTER_OFFSET_HI 				949391L
+#define VOLTSETTER_SLOPE_LO 				5559718L
+#define VOLTSETTER_OFFSET_LO 				164691L
+
+#define VOLTMETER_SLOPE_HI 					32011240L
+#define VOLTMETER_OFFSET_HI 				281L
+#define VOLTMETER_SLOPE_LO 					5562513L
+#define VOLTMETER_OFFSET_LO 				304L
+
+
+/* MightyWatt R3 parameters */
+
+#define BOARD_REVISION                   	"3.1.0"
+#define VOLTMETER_INPUT_RESISTANCE       	1000000000UL
+#define MAXIMUM_POWER                    	75000000UL
+
+/** 
+ * UNCOMMENT THE LINE THAT BELONGS TO YOUR ADC TYPE, COMMENT THE OTHER LINE
+ */
+ 
+//#define ADC_TYPE_ADS1015					        /* 12-bit */
+#define ADC_TYPE_ADS1115					        /* 16-bit */
+
+#if !defined(ADC_TYPE_ADS1015) && !defined(ADC_TYPE_ADS1115)
+  #error No ADC defined
+#elif (defined(ADC_TYPE_ADS1015) && defined(ADC_TYPE_ADS1115))
+  #error Only one type of ADC can be defined
+#endif
+
+#endif /* CONFIGURATION_H */
